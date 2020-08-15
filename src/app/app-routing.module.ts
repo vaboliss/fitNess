91c30c936e172bb12b689ac1bb66df3315aaeb97
Ainis <vabolis.ainis@gmail.com>
2020-08-15 12:34:03 +0300

    client-side

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { WorkoutsPageComponent } from './pages/workouts-page/workouts-page.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { AuthGuard } from './interceptors/auth.guard';
+import { WorkoutDetailsComponent } from './pages/workout-details/workout-details.component';
+import { WorkoutCreateComponent } from './pages/workout-create/workout-create.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+      {path: 'register', component: RegisterPageComponent},
+      {path: 'welcome', component: WelcomePageComponent},
+      {path: 'home', component: HomePageComponent, canActivate: [AuthGuard]},
+      {path: 'workouts', component: WorkoutsPageComponent, canActivate: [AuthGuard]},
+      {path: 'workouts/create', component: WorkoutCreateComponent, canActivate: [AuthGuard]},
+      {path: 'workouts/:id', component: WorkoutDetailsComponent, canActivate: [AuthGuard]},
+      {path: 'login', component: LoginPageComponent},
+      {path: '', redirectTo:'welcome', pathMatch: 'full'},
+      { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [LoginPageComponent,RegisterPageComponent,WelcomePageComponent,HomePageComponent,WorkoutsPageComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,56 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import {MaterialModule} from './app-material-import';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from './services/auth.service';
+import { RegisterService } from './services/register.service';
+import { RegisterDialogComponent } from './components/register-dialog.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { WorkoutService } from './services/workout.service';
+import { WorkoutTableComponent } from './components/workout-table/workout-table.component';
+import { DeleteDialogComponent } from './components/delete-dialog.component';
+import { WorkoutDetailsComponent } from './pages/workout-details/workout-details.component';
+import { WorkoutCreateComponent } from './pages/workout-create/workout-create.component';
+import { SchedulerModule } from '@progress/kendo-angular-scheduler';
+import { SchedulerComponent } from './components/scheduler/scheduler.component';
+import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { FormsModule } from '@angular/forms';
+import { WorkoutDaysService } from './services/workout-days.service';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents,
+    PageNotFoundComponent,
+    RegisterDialogComponent,
+    NavBarComponent,
+    WorkoutTableComponent,
+    DeleteDialogComponent,
+    WorkoutDetailsComponent,
+    WorkoutCreateComponent,
+    SchedulerComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    HttpClientModule,
+    SchedulerModule,
+    DateInputsModule,
+    DropDownsModule,
+    FormsModule
+
   ],
-  providers: [],
+  entryComponents: [RegisterDialogComponent,DeleteDialogComponent],
+  providers: [AuthService , RegisterService, WorkoutService, WorkoutDaysService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
